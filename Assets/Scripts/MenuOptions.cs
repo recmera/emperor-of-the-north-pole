deb87bfefc5cb3e57e85dc38e7af506ebe9db254
Ricardo Enrique Coronado <ricardo.coronado@alumnos.uach.cl>
2020-12-13 04:58:16 -0300

    main menu

---
+++ Assets/Scripts/MenuOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour{
+   
+   public void Play(){
+
+       SceneManager.LoadScene(1);
+   }
+   public void Exit(){
+
+       Application.Quit();
+   }
+}
